f9386986196404263f50898f6da8a132eb9aa71f
NeverSayNeverLove <NeverSayNeverLoveMD@gmail.com>
2019-08-06 01:30:14 +0700

    test signout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   root 'top_page#home'
   get '/help', to: 'top_page#help'
   devise_for :companies
-  devise_for :users
+  devise_for :users,
+    path: '',
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    controllers: {
+      sessions: "users/sessions",
+      registrations: "users/registrations",
+      passwords: "users/passwords",
+      confirmations: "users/confirmations"
+    }
 end
