8b553a19694d8836a114e29a1a39a26e1cc722b6
NeverSayNeverLove <NeverSayNeverLoveMD@gmail.com>
2019-08-06 11:57:44 +0700

    create devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'top_page#home'
   get '/help', to: 'top_page#help'
-  devise_for :companies
   devise_for :users,
     path: '',
-    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    path_names: {sign_in: 'login', sign_out: 'ogout', edit: 'profile', sign_up: 'registration'},
     controllers: {
       sessions: "users/sessions",
       registrations: "users/registrations",
