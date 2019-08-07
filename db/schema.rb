0bfdeae8fe6a4a16b84a98b1879e2fe66bb08954
EC2 Default User <ec2-user@ip-172-31-40-84.ec2.internal>
2019-08-07 07:25:25 +0000

    fix search box

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_063317) do
+ActiveRecord::Schema.define(version: 2019_08_07_041456) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_companies_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,7 +40,7 @@ ActiveRecord::Schema.define(version: 2019_08_06_063317) do
     t.string "title"
     t.integer "salary"
     t.integer "id_user"
-    t.integer "status"
+    t.string "status"
     t.string "skill"
     t.string "time"
     t.datetime "created_at", null: false
